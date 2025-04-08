61a786df49ad17a164af454903658156992e89aa
wesley.santos <wesley_sds@live.com>
2025-04-08 13:48:41 -0300

    Criado todos os endpoints

--- src/controllers/livroController.ts
+++ src/controllers/livroController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import db from '../db/db';
 import Livro from '../models/Livro';
+import Editora from '../models/Editora';
 
 // Retorna todos os livros e sua respectiva editora
 export const getAllLivros = async (req: Request, res: Response) => {
@@ -60,5 +61,221 @@ export const getLivro = async (req: Request, res: Response) => {
     res.status(200).json(livro);
   } catch (error) {
     res.status(500).json({ message: 'Erro ao buscar o livro', error });
+  }
+};
+
+// Busca a editora pelo ID
+export const getEditora = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  if (!(id > 0)) {
+    res.status(400).json({ message: "ID da editora inválido" });
+    return;
+  }
+
+  try {
+    const editora: Editora | null = await db.oneOrNone(`
+      SELECT 
+        Id,
+        Nome,
+        Cidade
+      FROM Editora
+      WHERE Id = $1
+    `, [id]);
+
+    if (!editora) {
+      res.status(404).json({ message: 'Editora não encontrado' });
+      return;
+    }
+    res.status(200).json(editora);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar a editora', error });
+  }
+};
+
+// Retorna todas as editoras
+export const getAllEditoras = async (req: Request, res: Response) => {
+  try {
+    const editoras: Editora[] = await db.any(`
+      SELECT Id, Nome, Cidade FROM Editora 
+    `);
+    res.status(200).json(editoras);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao buscar as editoras', error });
+  }
+};
+
+// Cria um novo livro
+export const createLivro = async (req: Request, res: Response) => {
+  const livro: Livro = req.body;
+
+  if (
+    !livro.Titulo ||
+    !livro.Autor ||
+    !livro.ISBN ||
+    !livro.AnoDePublicacao ||
+    !livro.Editora ||
+    !livro.Editora.Id
+  ) {
+    res.status(400).json({ message: 'Campos obrigatórios ausentes ou inválidos' });
+    return;
+  }  
+
+  try {
+    await db.none(`
+      INSERT INTO Livro (Titulo, Autor, ISBN, AnoDePublicacao, EditoraId)
+      VALUES ($1, $2, $3, $4, $5)
+      `, [livro.Titulo, livro.Autor, livro.ISBN, livro.AnoDePublicacao, livro.Editora.Id]);
+
+    res.status(201).json({ message: 'Livro criado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao criar livro', error });
+  }
+}
+
+// Cria uma nova editora
+export const createEditora = async (req: Request, res: Response) => {
+  const editora: Editora = req.body;
+
+  if (!editora.Nome || !editora.Cidade)  {
+    res.status(400).json({ message: 'Campos obrigatórios ausentes ou inválidos' });
+    return;
+  }
+
+  try {
+    await db.none(`
+      INSERT INTO Editora (Nome, Cidade)
+      VALUES ($1, $2)`, [editora.Nome, editora.Cidade]);
+
+    res.status(201).json({ message: 'Editora criada com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao criar editora', error });
+  }
+}
+
+// Atualizar Livro
+export const updateLivro = async (req: Request, res: Response) => {
+  const livro: Livro = req.body;
+
+  // Validação básica
+  if (
+    !livro.Id ||
+    !livro.Titulo ||
+    !livro.Autor ||
+    !livro.ISBN ||
+    !livro.AnoDePublicacao ||
+    !livro.Editora?.Id
+  ) {
+    res.status(400).json({ message: 'Campos obrigatórios ausentes ou inválidos' });
+    return;
+  }
+
+  try {
+    const result = await db.result(`
+      UPDATE Livro
+      SET Titulo = $1,
+          Autor = $2,
+          ISBN = $3,
+          AnoDePublicacao = $4,
+          EditoraId = $5
+      WHERE Id = $6
+    `, [
+      livro.Titulo,
+      livro.Autor,
+      livro.ISBN,
+      livro.AnoDePublicacao,
+      livro.Editora.Id,
+      livro.Id
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({ message: 'Livro não encontrado' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Livro atualizado com sucesso' });
+  } catch (error) {
+    console.error('Erro ao atualizar livro:', error);
+    res.status(500).json({ message: 'Erro ao atualizar livro', error });
+  }
+};
+
+//Atualizar Editora
+export const updateEditora = async (req: Request, res: Response) => {
+  const editora: Editora = req.body;
+
+  if (
+    !editora.Id ||
+    !editora.Nome ||
+    !editora.Cidade
+  ) {
+    res.status(400).json({ message: 'Campos obrigatórios ausentes ou inválidos' });
+    return;
+  }
+
+  try {
+    const result = await db.result(`
+      UPDATE Editora SET Nome = $1, Cidade = $2 WHERE Id = $3
+    `, [editora.Id]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({ message: 'Editora não encontrada' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Editora atualizada com sucesso' });
+  } catch (error) {
+    console.error('Erro ao atualizar editora:', error);
+    res.status(500).json({ message: 'Erro ao atualizar editora', error });
+  }
+};
+
+// Delete Livro
+export const deleteLivro = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  if (!(id > 0)) {
+    res.status(400).json({ message: 'ID inválido' });
+    return;
+  }
+
+  try {
+    const result = await db.result(`
+      DELETE FROM Livro WHERE Id = $1
+    `, [id]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({ message: 'Livro não encontrado' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Livro deletado com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao deletar livro', error });
+  }
+};
+
+// Delete Editora
+export const deleteEditora = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  if (!(id > 0)) {
+    res.status(400).json({ message: 'ID inválido' });
+    return;
+  }
+
+  try {
+    const result = await db.result(`
+      DELETE FROM Editora WHERE Id = $1
+    `, [id]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({ message: 'Editora não encontrada' });
+      return 
+    }
+
+    res.status(200).json({ message: 'Editora deletada com sucesso' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao deletar editora', error });
   }
 };
--- src/routes/livroRoutes.ts
+++ src/routes/livroRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from 'express';
-import { getAllLivros, getLivro } from '../controllers/livroController';
+import { getAllLivros, getLivro, createLivro, createEditora, getAllEditoras, getEditora, deleteLivro, deleteEditora, updateLivro, updateEditora } from '../controllers/livroController';
 
 const router = Router();
 
-router.get('/livros', getAllLivros);
+//GET
 router.get('/livro/:id', getLivro);
-    
+router.get('/livros', getAllLivros);
+router.get('/Editora/:id', getEditora);
+router.get('/Editoras', getAllEditoras);
+
+//POST 
+router.post('/livro/new', createLivro);
+router.post('/editora/new', createEditora);
+
+//PUT 
+router.put('/livro/update', updateLivro);
+router.put('/editora/update', updateEditora);
+
+//DELETE 
+router.delete('livro/remove', deleteLivro);
+router.delete('editora/remove', deleteEditora);
+
 export default router;
